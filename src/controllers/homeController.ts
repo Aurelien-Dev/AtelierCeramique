6dc630fd2661559d8e04f5b4e715eeb4d2d64ff0
Aurelien <a.cretaine@live.fr>
2021-06-04 14:41:40 +0200

    Délestage des tests préalables

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,22 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-import async from 'async'
-import ShopifyUtils from '../services/shopify/shopifyService';
-import client from '../services/shopify/shopifyClient'
-
-
-
-export async function initShopify(req: Request, res: Response, next: NextFunction) {
-    next()
-}
 
 export function index(req: Request, res: Response, next: NextFunction) {
-    res.render('home/index', {
-    
-    });
+    res.render('home/index');
 }
 
 export function pos(req: Request, res: Response, next: NextFunction) {
-    res.render('home/pos', {
-    
-    });
+    res.render('home/pos');
 }
--- src/routers/homeRouter.ts
+++ src/routers/homeRouter.ts
@@ -4,7 +4,6 @@ import * as homeController from '../controllers/homeController';
 // Create a new router to handle /movies routes
 const homeRouter = Router();
 
-homeRouter.use(homeController.initShopify)
 homeRouter.get('/', homeController.index)
 homeRouter.get('/point-de-vente', homeController.pos)
 
