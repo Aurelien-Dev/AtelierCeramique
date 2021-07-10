8ec1a095bc302290c1c3a10236fdc67bb6e198b6
Aurelien <a.cretaine@live.fr>
2021-07-10 11:43:02 +0200

    Correction faute frappe

--- src/server.ts
+++ src/server.ts
@@ -13,19 +13,6 @@ export default class Server {
 
   initEnvironement() {
     dotenv.config({ path: `${__dirname}/.env` });
-
-    // let path;
-    // switch (process.env.NODE_ENV) {
-    //   case "test":
-    //     path = `${__dirname}/../../.env.test`;
-    //     break;
-    //   case "production":
-    //     path = `${__dirname}/../../.env.production`;
-    //     break;
-    //   default:
-    //     path = `${__dirname}/../../.env.development`;
-    // }
-    // dotenv.config({ path: path });
   }
 
   /**
